78c15fe7ac1404b916cd438dcf98218a4a00e680
Luis <lvarg009@ucr.edu>
2020-03-15 21:39:42 -0700

    Added current version of query file
    Query gets all Lecture instances of classes that student can take.

--- firebaseQueryTest01.py
+++ firebaseQueryTest01.py
@@ -1,10 +1,36 @@
 #Function that adds documents to Firebase collection
+
+#To make this work, make sure to have already run testFireBaseInit.py,
+#or copy/paste all contents of that file after this file's 'from' statements
+#Also, make sure URPlanClasses.py and designProjectScratch.py are in the 
+#same project directory where you'll run this.
+
 from URPlanClasses import Student
 from URPlanClasses import Class
 
+from designProjectScratch import ClassInfo
+from designProjectScratch import initCSCoreClass
+from designProjectScratch import initCSNonCoreClasses
+
+#doc_refStudent = db.collection(u'students').document(u'864651254')
+#doc_refClass = db.collection(u'classes').document(u'53095') #Currently example query set to CS014 class. 
+
+
+#try:
+#    docStudent = doc_refStudent.get()
+#    studentQuery = Student.from_dict(docStudent.to_dict())
+#    print(studentQuery)
+#except google.cloud.exceptions.NotFound:
+#    print("Student Document Not Found")
+
+#try:
+#    docClass = doc_refClass.get()
+#    classQuery = Class.from_dict(docClass.to_dict())
+#    print(classQuery)
+#except google.cloud.exceptions.NotFound:
+#    print("Document Not Found")
 
 doc_refStudent = db.collection(u'students').document(u'864651254')
-doc_refClass = db.collection(u'classes').document(u'53095') #Currently example query set to CS014 class. 
 
 try:
     docStudent = doc_refStudent.get()
@@ -13,18 +39,60 @@ try:
 except google.cloud.exceptions.NotFound:
     print("Student Document Not Found")
 
-try:
-    docClass = doc_refClass.get()
-    classQuery = Class.from_dict(docClass.to_dict())
-    print(classQuery)
-except google.cloud.exceptions.NotFound:
-    print("Document Not Found")
+studentList = studentQuery.coursesList[:]
 
-#if(not studentQuery.coursesList):
-    #List empty - Likely a new student - Recommend default starting classes
-#else:
-    #Have a copy of a - or some - 'master list(s)' that compares against a student's coursesList.
-    #Remove classes that are in student's coursesList from master list copies. 
-    #Create a graph in Python using the remaining  
+coreClasses = initCSCoreClass()
+nonCoreClasses = initCSNonCoreClasses()
 
-    #TA suggests that it will be a DFS issue.
+classesToQuery = []
+
+for selClass in studentList:
+    for classObj in coreClasses[:]:
+        if(selClass == classObj.className):
+            coreClasses.remove(classObj)
+
+for selClass in studentList:
+    for classObj in nonCoreClasses[:]:
+        if(selClass == classObj.className):
+            nonCoreClasses.remove(classObj)
+
+for selClass in studentList:
+    for classObj in coreClasses[:]:
+        if(selClass in classObj.preReqtoTake):
+            classObj.preReqtoTake.remove(selClass)
+
+for selClass2 in studentList:
+    for classObj2 in nonCoreClasses[:]:
+        if(selClass2 in classObj2.preReqtoTake):
+            classObj2.preReqtoTake.remove(selClass2)
+
+print("Core classes that you can take after classes removal: ")
+for classObj in coreClasses:
+    if(not classObj.preReqtoTake):
+        print(classObj.className + " can now be taken!")
+        classesToQuery.append(classObj.className)
+
+print("Classes in nonCore that can be taken")
+
+for classObj2 in nonCoreClasses:
+    if(not classObj2.preReqtoTake):
+        print(classObj2.className + " can now be taken!")
+        classesToQuery.append(classObj2.className)
+
+classes_Ref = db.collection(u'classes')
+queryQueue = []
+for classItem in classesToQuery:
+    queryQueue.append(classes_Ref.where(u'courseName', u'==', classItem).where(u'classType', u'==', u'LEC'))
+
+listOfClasses = []
+for queryItem in queryQueue:
+    try:
+        docs = queryItem.stream()
+        for doc in docs:
+            listOfClasses.append(Class.from_dict(doc.to_dict()))
+    except google.cloud.exceptions.NotFound:
+        print("Doc not found")
+
+print("Results (in Class object form)")
+for item in listOfClasses:
+    print(item)
